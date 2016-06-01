26475417c065a8d4fbbbed63624130f091e09028
kalai <kalai@kalaimac.local>
2016-05-31 22:09:29 -0700

    parking finder can claim available spots

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -2,13 +2,25 @@ class SpotsController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
 	def index
-		render json: {spots: Spot.all, total: Spot.count}, except: [:created_at, :updated_at]
+		spots = Spot.where(target_id: nil)
+		if current_user
+			id = 1 #TODO#
+		end
+		targets = Spot.where(target_id: id)
+		render json: {spots: spots, targets: targets, total: spots.count + targets.count}, except: [:created_at, :updated_at]
 	end
 
 	def create
 		spot = Spot.new(spot_params)
 		spot.save!
 		render json: {spot: spot, total: Spot.count}, except: [:created_at, :updated_at]
+	end
+
+	def update
+		spot = Spot.find_by_id(params[:id])
+		spot.target_id = (params[:target_id] ? params[:target_id] : nil)
+		spot.save!
+		render json: spot, except: [:created_at, :updated_at]
 	end
 
 	def destroy
